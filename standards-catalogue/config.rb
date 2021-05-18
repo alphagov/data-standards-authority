5420baf7949b206760041645a922d0caaf717abc
charlesbaird-dsa <68249424+charlesbaird-dsa@users.noreply.github.com>
2021-05-18 09:30:53 +0100

    Update config.rb

--- standards-catalogue/config.rb
+++ standards-catalogue/config.rb
@@ -104,7 +104,7 @@ helpers UrlHelpers
 
 # Order organisations from A-Z in the Table of Contents,
 # leaving a buffer from 0-999 for static content to be given priority
-# initial_org_weight = 1_000
+initial_org_weight = 1_000
 
 # api_catalogue.organisations_apis.each.with_index(initial_org_weight) do |(organisation, apis), org_weight|
 #   proxy(
