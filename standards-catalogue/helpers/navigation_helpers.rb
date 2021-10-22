bee02ad1067afd61e1fa392717771fe0ba04c5fe
Bethan Palmer <bethan.palmer@hotmail.com>
2021-10-22 16:49:49 +0100

    Don't include root page in nav bar
    Checking if the page includes 'DSA' could have led to returning nil.
    This is actually the root page so the simplest way to avoid this is not
    explicitly include it.

--- standards-catalogue/helpers/navigation_helpers.rb
+++ standards-catalogue/helpers/navigation_helpers.rb
@@ -3,8 +3,6 @@ module NavigationHelpers
     root_page = resources.detect { |r| r.path == "index.html" }
     top_level_pages = navigable_pages(root_page.children).sort_by { |page| page.data.weight || 99_999 }
     navigation = []
-
-    navigation << navigation_html(root_page)
     navigation += top_level_pages.map { |page| navigation_html(page) }
     navigation.join
   end
@@ -18,13 +16,11 @@ private
   end
 
   def navigation_html(page)
-    unless page.data.title.include? "DSA"
-      <<~HTML
-        <li>
-          <a href="#{page.url}"><span>#{page.data.title}</span></a>
-          #{yield if block_given?}
-        </li>
-      HTML
-    end
+    <<~HTML
+      <li>
+        <a href="#{page.url}"><span>#{page.data.title}</span></a>
+        #{yield if block_given?}
+      </li>
+    HTML
   end
 end
