10841e7a21f6d2e1b1e72179f656c45162b3ba93
Jamie Tanna <jamie@jamietanna.co.uk>
2021-10-25 11:55:51 +0100

    Remove API Catalogue code
    As we've still got a few bits from the API Catalogue, we should remove
    them to clean the codebase up a bit more.

--- standards-catalogue/config.rb
+++ standards-catalogue/config.rb
@@ -1,5 +1,3 @@
-require "lib/url_helpers"
-
 ### Config from tech-docs-gem: start ###
 require "middleman"
 require "middleman-autoprefixer"
@@ -122,53 +120,3 @@ activate :search do |search|
   search.tokenizer_separator = '/[\s\-/]+/'
 end
 ### Config from tech-docs-gem: end ###
-
-helpers UrlHelpers
-
-# catalogue_csv = File.expand_path("data/catalogue.csv", __dir__)
-# organisation_csv = File.expand_path("data/organisation.csv", __dir__)
-# api_catalogue = ApiCatalogue.from_csv(catalogue_csv: catalogue_csv, organisation_csv: organisation_csv)
-
-# Order organisations from A-Z in the Table of Contents,
-# leaving a buffer from 0-999 for static content to be given priority
-# initial_org_weight = 1_000
-
-# api_catalogue.organisations_apis.each.with_index(initial_org_weight) do |(organisation, apis), org_weight|
-#   proxy(
-#     UrlHelpers.organisation_path(organisation),
-#     "organisation_index.html",
-#     locals: { organisation: organisation, apis: apis },
-#     data: {
-#       title: organisation.name,
-#       weight: org_weight,
-#     },
-#     ignore: true,
-#   )
-
-#   apis.each_with_index do |api, api_weight|
-#     proxy(
-#       UrlHelpers.api_path(organisation: organisation, api: api),
-#       "api_details.html",
-#       locals: { api: api },
-#       data: {
-#         title: api.name,
-#         weight: api_weight,
-#       },
-#       ignore: true,
-#     )
-#   end
-# end
-
-# proxy(
-#   "/dashboard/index.html",
-#   "dashboard.html",
-#   locals: { dashboard_stats: DashboardStats.new(api_catalogue) },
-#   ignore: true,
-# )
-
-# proxy(
-#   "/index/index.html",
-#   "overview_index.html",
-#   locals: { overview: ApiCatalogueOverview.new(api_catalogue) },
-#   ignore: true,
-# )
